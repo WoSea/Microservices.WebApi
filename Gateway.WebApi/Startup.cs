f804073c7e59b3431795c0fd6fec58a61387cfd1
duy.pham <duy.pham@logixtek.com>
2020-11-26 17:33:10 +0700

    Complete

--- Gateway.WebApi/Startup.cs
+++ Gateway.WebApi/Startup.cs
@@ -1,11 +1,9 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting; 
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware; 
 using System.Threading.Tasks;
 
 namespace Gateway.WebApi
@@ -15,11 +13,12 @@ namespace Gateway.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
-        {
+        { 
+            services.AddOcelot();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -30,11 +29,9 @@ namespace Gateway.WebApi
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
-        }
+            await app.UseOcelot();
+        } 
     }
 }
